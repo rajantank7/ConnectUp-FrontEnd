1a083f8545db3f3c5724aa5a3d3753d174470c75
rajan tank <rajantank7@gmail.com>
2025-07-03 21:05:47 +0530

    done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
 import UserCard from "./UserCard";
